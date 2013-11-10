dcc3ccf362442d7e99b919bb5506d3fab1620a42
Roy Clarkson <rclarkson@gopivotal.com>
2013-11-10 00:29:46 -0600

    Update complete POM to use the Maven Shade Plugin

--- test/run.sh
+++ test/run.sh
@@ -6,7 +6,22 @@ cd ../complete
 mvn clean package
 ret=$?
 if [ $ret -ne 0 ]; then
-exit $ret
+    exit $ret
+fi
+
+java -jar target/gs-maven-0.1.0.jar | tail -1 > target/actual.txt
+
+echo "Let's look at the actual results: `cat target/actual.txt`"
+echo "And compare it to: `cat ../test/expected.txt`"
+
+if diff -w ../test/expected.txt target/actual.txt
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
 fi
 rm -rf target
 
@@ -15,7 +30,7 @@ cd ../initial
 mvn clean package
 ret=$?
 if [ $ret -ne 0 ]; then
-exit $ret
+    exit $ret
 fi
 rm -rf target
 
