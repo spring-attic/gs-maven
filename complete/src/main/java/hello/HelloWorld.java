8f167bdb3b1a8890d82e5834832feba428aaa268
Dave Syer <dsyer@pivotal.io>
2016-08-25 10:36:07 +0100

    Add a test

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
