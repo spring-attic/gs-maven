311eb5064cd293c17484d880ade3052e16431d8f
Govind Kumar <35293008+govind144@users.noreply.github.com>
2021-10-07 17:19:36 +0530

    Update HelloWorld.java

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -2,6 +2,8 @@ package hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		
+		// create object for Greeter class
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
