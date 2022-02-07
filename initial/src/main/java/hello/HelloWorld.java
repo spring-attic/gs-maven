bb551f0a6b8c13856fe25705519eaca84c40fb2c
Catalin <c.neamtu@dxc.com>
2022-02-07 21:02:15 +0200

    first commit

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -1,8 +1,8 @@
 package hello;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHello());
-	}
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
 }
