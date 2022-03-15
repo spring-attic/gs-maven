fffdad0ec392043f21726c7c7b6f62636cb08c4b
Tenghuan He <tenghuanhe@gmail.com>
2022-03-15 12:06:34 +0800

    break test

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
 	@Test
 	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("Hello"));
+		assertThat(greeter.sayHello(), containsString("Hello ci"));
 	}
 
 }
