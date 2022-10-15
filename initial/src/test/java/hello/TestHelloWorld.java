e4aa5d55ca2eb488100b570d726d6cc9745e44c1
Hyeonykim <walnbobbbol@gmail.com>
2022-10-15 21:17:27 +0900

    add testcase
    Signed-off-by: Hyeonykim <walnbobbbol@gmail.com>

---
+++ initial/src/test/java/hello/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHelloWorld {
+    @Test
+    public void testExample() {
+        assertEquals(1,1);
+    }
+}
